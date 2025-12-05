121a615b6efd0eb3cf3ddb361097108b41e85c38
jonathanrlee2023 <johnnyrlee05@gmail.com>
2025-12-05 14:45:13 -0600

    Filtered down data

--- main.py
+++ main.py
@@ -12,6 +12,14 @@ if __name__ == "__main__":
     df = df.reset_index(drop=True)
     pd.set_option('display.max_colwidth', None)
 
-    print(df.head(10))
-    print(len(df))
-    print(df["Data_Value"].max())
+    df['Sample_Size'] = df['Sample_Size'].replace({',': ''}, regex=True).astype(float)
+    df['Weighted'] = np.floor((df['Data_Value']/100.0) * df['Sample_Size'])
+
+    result = (
+        df.groupby(['LocationAbbr', 'YearStart'])[['Weighted','Sample_Size']]
+        .apply(lambda g: g['Weighted'].sum() / g['Sample_Size'].sum())
+        .reset_index(name='ObesityRate')
+    )
+    row = result.loc[result['ObesityRate'].idxmax()]
+    print(row)
+    
